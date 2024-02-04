fd503e7978e9f017b1e7a15a421a4882257a305d
Mustafa Gönültaş <gonultasmf@hotmail.com>
2024-02-04 20:46:55 +0300

    FmgLib.MauiMarkup.Views: Behaviors Extension added

---
+++ libs/FmgLib.MauiMarkup.Views/Extensions/ProgressBarAnimationBehaviorExtension.cs
@@ -0,0 +1,97 @@
+﻿using CommunityToolkit.Maui.Animations;
+using CommunityToolkit.Maui.Behaviors;
+
+namespace FmgLib.MauiMarkup.Views;
+
+public static partial class ProgressBarAnimationBehaviorExtension
+{
+    public static T ProgressFmg<T>(this T self, double progress) where T : ProgressBarAnimationBehavior
+    {
+        self.SetValue(ProgressBarAnimationBehavior.ProgressProperty, progress);
+        return self;
+    }
+
+    public static T ProgressFmg<T>(this T self, Func<PropertyContext<double>, IPropertyBuilder<double>> configure) where T : ProgressBarAnimationBehavior
+    {
+        PropertyContext<double> arg = new PropertyContext<double>(self, ProgressBarAnimationBehavior.ProgressProperty);
+        configure(arg).Build();
+        return self;
+    }
+
+    public static SettersContext<T> ProgressFmg<T>(this SettersContext<T> self, double progress) where T : ProgressBarAnimationBehavior
+    {
+        self.XamlSetters.Add(new Setter
+        {
+            Property = ProgressBarAnimationBehavior.ProgressProperty,
+            Value = progress
+        });
+        return self;
+    }
+
+    public static SettersContext<T> ProgressFmg<T>(this SettersContext<T> self, Func<PropertySettersContext<double>, IPropertySettersBuilder<double>> configure) where T : ProgressBarAnimationBehavior
+    {
+        PropertySettersContext<double> arg = new PropertySettersContext<double>(self.XamlSetters, ProgressBarAnimationBehavior.ProgressProperty);
+        configure(arg).Build();
+        return self;
+    }
+
+    public static T LengthFmg<T>(this T self, uint length) where T : ProgressBarAnimationBehavior
+    {
+        self.SetValue(ProgressBarAnimationBehavior.LengthProperty, length);
+        return self;
+    }
+
+    public static T LengthFmg<T>(this T self, Func<PropertyContext<uint>, IPropertyBuilder<uint>> configure) where T : ProgressBarAnimationBehavior
+    {
+        PropertyContext<uint> arg = new PropertyContext<uint>(self, ProgressBarAnimationBehavior.LengthProperty);
+        configure(arg).Build();
+        return self;
+    }
+
+    public static SettersContext<T> LengthFmg<T>(this SettersContext<T> self, uint length) where T : ProgressBarAnimationBehavior
+    {
+        self.XamlSetters.Add(new Setter
+        {
+            Property = ProgressBarAnimationBehavior.LengthProperty,
+            Value = length
+        });
+        return self;
+    }
+
+    public static SettersContext<T> LengthFmg<T>(this SettersContext<T> self, Func<PropertySettersContext<uint>, IPropertySettersBuilder<uint>> configure) where T : ProgressBarAnimationBehavior
+    {
+        PropertySettersContext<uint> arg = new PropertySettersContext<uint>(self.XamlSetters, ProgressBarAnimationBehavior.LengthProperty);
+        configure(arg).Build();
+        return self;
+    }
+
+    public static T EasingFmg<T>(this T self, Easing easing) where T : ProgressBarAnimationBehavior
+    {
+        self.SetValue(ProgressBarAnimationBehavior.EasingProperty, easing);
+        return self;
+    }
+
+    public static T EasingFmg<T>(this T self, Func<PropertyContext<Easing>, IPropertyBuilder<Easing>> configure) where T : ProgressBarAnimationBehavior
+    {
+        PropertyContext<Easing> arg = new PropertyContext<Easing>(self, ProgressBarAnimationBehavior.EasingProperty);
+        configure(arg).Build();
+        return self;
+    }
+
+    public static SettersContext<T> EasingFmg<T>(this SettersContext<T> self, Easing easing) where T : ProgressBarAnimationBehavior
+    {
+        self.XamlSetters.Add(new Setter
+        {
+            Property = ProgressBarAnimationBehavior.EasingProperty,
+            Value = easing
+        });
+        return self;
+    }
+
+    public static SettersContext<T> EasingFmg<T>(this SettersContext<T> self, Func<PropertySettersContext<Easing>, IPropertySettersBuilder<Easing>> configure) where T : ProgressBarAnimationBehavior
+    {
+        PropertySettersContext<Easing> arg = new PropertySettersContext<Easing>(self.XamlSetters, ProgressBarAnimationBehavior.EasingProperty);
+        configure(arg).Build();
+        return self;
+    }
+}
---
+++ libs/FmgLib.MauiMarkup.Views/Extensions/UserStoppedTypingBehaviorExtension.cs
@@ -0,0 +1,157 @@
+﻿using CommunityToolkit.Maui.Behaviors;
+using System.Windows.Input;
+
+namespace FmgLib.MauiMarkup.Views;
+
+public static partial class UserStoppedTypingBehaviorExtension
+{
+    public static T CommandFmg<T>(this T self, ICommand command) where T : UserStoppedTypingBehavior
+    {
+        self.SetValue(UserStoppedTypingBehavior.CommandProperty, command);
+        return self;
+    }
+
+    public static T CommandFmg<T>(this T self, Func<PropertyContext<ICommand>, IPropertyBuilder<ICommand>> configure) where T : UserStoppedTypingBehavior
+    {
+        PropertyContext<ICommand> arg = new PropertyContext<ICommand>(self, UserStoppedTypingBehavior.CommandProperty);
+        configure(arg).Build();
+        return self;
+    }
+
+    public static SettersContext<T> CommandFmg<T>(this SettersContext<T> self, ICommand command) where T : UserStoppedTypingBehavior
+    {
+        self.XamlSetters.Add(new Setter
+        {
+            Property = UserStoppedTypingBehavior.CommandProperty,
+            Value = command
+        });
+        return self;
+    }
+
+    public static SettersContext<T> CommandFmg<T>(this SettersContext<T> self, Func<PropertySettersContext<ICommand>, IPropertySettersBuilder<ICommand>> configure) where T : UserStoppedTypingBehavior
+    {
+        PropertySettersContext<ICommand> arg = new PropertySettersContext<ICommand>(self.XamlSetters, UserStoppedTypingBehavior.CommandProperty);
+        configure(arg).Build();
+        return self;
+    }
+
+    public static T CommandParameterFmg<T>(this T self, object value) where T : UserStoppedTypingBehavior
+    {
+        self.SetValue(UserStoppedTypingBehavior.CommandParameterProperty, value);
+        return self;
+    }
+
+    public static T CommandParameterFmg<T>(this T self, Func<PropertyContext<object>, IPropertyBuilder<object>> configure) where T : UserStoppedTypingBehavior
+    {
+        PropertyContext<object> arg = new PropertyContext<object>(self, UserStoppedTypingBehavior.CommandParameterProperty);
+        configure(arg).Build();
+        return self;
+    }
+
+    public static SettersContext<T> CommandParameterFmg<T>(this SettersContext<T> self, object value) where T : UserStoppedTypingBehavior
+    {
+        self.XamlSetters.Add(new Setter
+        {
+            Property = UserStoppedTypingBehavior.CommandParameterProperty,
+            Value = value
+        });
+        return self;
+    }
+
+    public static SettersContext<T> CommandParameterFmg<T>(this SettersContext<T> self, Func<PropertySettersContext<object>, IPropertySettersBuilder<object>> configure) where T : UserStoppedTypingBehavior
+    {
+        PropertySettersContext<object> arg = new PropertySettersContext<object>(self.XamlSetters, UserStoppedTypingBehavior.CommandParameterProperty);
+        configure(arg).Build();
+        return self;
+    }
+
+    public static T StoppedTypingTimeThresholdFmg<T>(this T self, int count) where T : UserStoppedTypingBehavior
+    {
+        self.SetValue(UserStoppedTypingBehavior.StoppedTypingTimeThresholdProperty, count);
+        return self;
+    }
+
+    public static T StoppedTypingTimeThresholdFmg<T>(this T self, Func<PropertyContext<int>, IPropertyBuilder<int>> configure) where T : UserStoppedTypingBehavior
+    {
+        PropertyContext<int> arg = new PropertyContext<int>(self, UserStoppedTypingBehavior.StoppedTypingTimeThresholdProperty);
+        configure(arg).Build();
+        return self;
+    }
+
+    public static SettersContext<T> StoppedTypingTimeThresholdFmg<T>(this SettersContext<T> self, int count) where T : UserStoppedTypingBehavior
+    {
+        self.XamlSetters.Add(new Setter
+        {
+            Property = UserStoppedTypingBehavior.StoppedTypingTimeThresholdProperty,
+            Value = count
+        });
+        return self;
+    }
+
+    public static SettersContext<T> StoppedTypingTimeThresholdFmg<T>(this SettersContext<T> self, Func<PropertySettersContext<int>, IPropertySettersBuilder<int>> configure) where T : UserStoppedTypingBehavior
+    {
+        PropertySettersContext<int> arg = new PropertySettersContext<int>(self.XamlSetters, UserStoppedTypingBehavior.StoppedTypingTimeThresholdProperty);
+        configure(arg).Build();
+        return self;
+    }
+
+    public static T MinimumLengthThresholdFmg<T>(this T self, int minLength) where T : UserStoppedTypingBehavior
+    {
+        self.SetValue(UserStoppedTypingBehavior.MinimumLengthThresholdProperty, minLength);
+        return self;
+    }
+
+    public static T MinimumLengthThresholdFmg<T>(this T self, Func<PropertyContext<int>, IPropertyBuilder<int>> configure) where T : UserStoppedTypingBehavior
+    {
+        PropertyContext<int> arg = new PropertyContext<int>(self, UserStoppedTypingBehavior.MinimumLengthThresholdProperty);
+        configure(arg).Build();
+        return self;
+    }
+
+    public static SettersContext<T> MinimumLengthThresholdFmg<T>(this SettersContext<T> self, int minLength) where T : UserStoppedTypingBehavior
+    {
+        self.XamlSetters.Add(new Setter
+        {
+            Property = UserStoppedTypingBehavior.MinimumLengthThresholdProperty,
+            Value = minLength
+        });
+        return self;
+    }
+
+    public static SettersContext<T> MinimumLengthThresholdFmg<T>(this SettersContext<T> self, Func<PropertySettersContext<int>, IPropertySettersBuilder<int>> configure) where T : UserStoppedTypingBehavior
+    {
+        PropertySettersContext<int> arg = new PropertySettersContext<int>(self.XamlSetters, UserStoppedTypingBehavior.MinimumLengthThresholdProperty);
+        configure(arg).Build();
+        return self;
+    }
+
+    public static T ShouldDismissKeyboardAutomaticallyFmg<T>(this T self, bool shouldDismissKeyboardAutomatically) where T : UserStoppedTypingBehavior
+    {
+        self.SetValue(UserStoppedTypingBehavior.ShouldDismissKeyboardAutomaticallyProperty, shouldDismissKeyboardAutomatically);
+        return self;
+    }
+
+    public static T ShouldDismissKeyboardAutomaticallyFmg<T>(this T self, Func<PropertyContext<bool>, IPropertyBuilder<bool>> configure) where T : UserStoppedTypingBehavior
+    {
+        PropertyContext<bool> arg = new PropertyContext<bool>(self, UserStoppedTypingBehavior.ShouldDismissKeyboardAutomaticallyProperty);
+        configure(arg).Build();
+        return self;
+    }
+
+    public static SettersContext<T> ShouldDismissKeyboardAutomaticallyFmg<T>(this SettersContext<T> self, bool shouldDismissKeyboardAutomatically) where T : UserStoppedTypingBehavior
+    {
+        self.XamlSetters.Add(new Setter
+        {
+            Property = UserStoppedTypingBehavior.ShouldDismissKeyboardAutomaticallyProperty,
+            Value = shouldDismissKeyboardAutomatically
+        });
+        return self;
+    }
+
+    public static SettersContext<T> ShouldDismissKeyboardAutomaticallyFmg<T>(this SettersContext<T> self, Func<PropertySettersContext<bool>, IPropertySettersBuilder<bool>> configure) where T : UserStoppedTypingBehavior
+    {
+        PropertySettersContext<bool> arg = new PropertySettersContext<bool>(self.XamlSetters, UserStoppedTypingBehavior.ShouldDismissKeyboardAutomaticallyProperty);
+        configure(arg).Build();
+        return self;
+    }
+}

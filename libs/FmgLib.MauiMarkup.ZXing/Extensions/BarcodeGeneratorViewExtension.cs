9f8d1fbb2405f1473b9ee459d919efd6ba33b49a
Mustafa Gönültaş <gonultasmf@hotmail.com>
2024-02-22 22:33:38 +0300

    FmgLib.MauiMarkup.ZXing published.

---
+++ libs/FmgLib.MauiMarkup.ZXing/Extensions/BarcodeGeneratorViewExtension.cs
@@ -0,0 +1,167 @@
+﻿using ZXing.Net.Maui;
+using ZXing.Net.Maui.Controls;
+
+namespace FmgLib.MauiMarkup.ZXing;
+
+public static partial class BarcodeGeneratorViewExtension
+{
+    public static T FormatFmg<T>(this T self,
+        BarcodeFormat format)
+        where T : BarcodeGeneratorView
+    {
+        self.SetValue(BarcodeGeneratorView.FormatProperty, format);
+        return self;
+    }
+
+    public static T FormatFmg<T>(this T self, Func<PropertyContext<BarcodeFormat>, IPropertyBuilder<BarcodeFormat>> configure)
+        where T : BarcodeGeneratorView
+    {
+        var context = new PropertyContext<BarcodeFormat>(self, BarcodeGeneratorView.FormatProperty);
+        configure(context).Build();
+        return self;
+    }
+
+    public static SettersContext<T> FormatFmg<T>(this SettersContext<T> self,
+        BarcodeFormat format)
+        where T : BarcodeGeneratorView
+    {
+        self.XamlSetters.Add(new Setter { Property = BarcodeGeneratorView.FormatProperty, Value = format });
+        return self;
+    }
+
+    public static SettersContext<T> FormatFmg<T>(this SettersContext<T> self, Func<PropertySettersContext<BarcodeFormat>, IPropertySettersBuilder<BarcodeFormat>> configure)
+        where T : BarcodeGeneratorView
+    {
+        var context = new PropertySettersContext<BarcodeFormat>(self.XamlSetters, BarcodeGeneratorView.FormatProperty);
+        configure(context).Build();
+        return self;
+    }
+
+    public static T ValueFmg<T>(this T self,
+        string value)
+        where T : BarcodeGeneratorView
+    {
+        self.SetValue(BarcodeGeneratorView.ValueProperty, value);
+        return self;
+    }
+
+    public static T ValueFmg<T>(this T self, Func<PropertyContext<string>, IPropertyBuilder<string>> configure)
+        where T : BarcodeGeneratorView
+    {
+        var context = new PropertyContext<string>(self, BarcodeGeneratorView.ValueProperty);
+        configure(context).Build();
+        return self;
+    }
+
+    public static SettersContext<T> ValueFmg<T>(this SettersContext<T> self,
+        string value)
+        where T : BarcodeGeneratorView
+    {
+        self.XamlSetters.Add(new Setter { Property = BarcodeGeneratorView.ValueProperty, Value = value });
+        return self;
+    }
+
+    public static SettersContext<T> ValueFmg<T>(this SettersContext<T> self, Func<PropertySettersContext<string>, IPropertySettersBuilder<string>> configure)
+        where T : BarcodeGeneratorView
+    {
+        var context = new PropertySettersContext<string>(self.XamlSetters, BarcodeGeneratorView.ValueProperty);
+        configure(context).Build();
+        return self;
+    }
+
+    public static T ForegroundColorFmg<T>(this T self,
+        Color color)
+        where T : BarcodeGeneratorView
+    {
+        self.SetValue(BarcodeGeneratorView.ForegroundColorProperty, color);
+        return self;
+    }
+
+    public static T ForegroundColorFmg<T>(this T self, Func<PropertyContext<Color>, IPropertyBuilder<Color>> configure)
+        where T : BarcodeGeneratorView
+    {
+        var context = new PropertyContext<Color>(self, BarcodeGeneratorView.ForegroundColorProperty);
+        configure(context).Build();
+        return self;
+    }
+
+    public static SettersContext<T> ForegroundColorFmg<T>(this SettersContext<T> self,
+        Color color)
+        where T : BarcodeGeneratorView
+    {
+        self.XamlSetters.Add(new Setter { Property = BarcodeGeneratorView.ForegroundColorProperty, Value = color });
+        return self;
+    }
+
+    public static SettersContext<T> ForegroundColorFmg<T>(this SettersContext<T> self, Func<PropertySettersContext<Color>, IPropertySettersBuilder<Color>> configure)
+        where T : BarcodeGeneratorView
+    {
+        var context = new PropertySettersContext<Color>(self.XamlSetters, BarcodeGeneratorView.ForegroundColorProperty);
+        configure(context).Build();
+        return self;
+    }
+
+    public static T BackgroundColorFmg<T>(this T self,
+        Color backgroundColor)
+        where T : BarcodeGeneratorView
+    {
+        self.SetValue(BarcodeGeneratorView.BackgroundColorProperty, backgroundColor);
+        return self;
+    }
+
+    public static T BackgroundColorFmg<T>(this T self, Func<PropertyContext<Color>, IPropertyBuilder<Color>> configure)
+        where T : BarcodeGeneratorView
+    {
+        var context = new PropertyContext<Color>(self, BarcodeGeneratorView.BackgroundColorProperty);
+        configure(context).Build();
+        return self;
+    }
+
+    public static SettersContext<T> BackgroundColorFmg<T>(this SettersContext<T> self,
+        Color backgroundColor)
+        where T : BarcodeGeneratorView
+    {
+        self.XamlSetters.Add(new Setter { Property = BarcodeGeneratorView.BackgroundColorProperty, Value = backgroundColor });
+        return self;
+    }
+
+    public static SettersContext<T> BackgroundColorFmg<T>(this SettersContext<T> self, Func<PropertySettersContext<Color>, IPropertySettersBuilder<Color>> configure)
+        where T : BarcodeGeneratorView
+    {
+        var context = new PropertySettersContext<Color>(self.XamlSetters, BarcodeGeneratorView.BackgroundColorProperty);
+        configure(context).Build();
+        return self;
+    }
+
+    public static T BarcodeMarginFmg<T>(this T self,
+        int barcodeMargin)
+        where T : BarcodeGeneratorView
+    {
+        self.SetValue(BarcodeGeneratorView.BarcodeMarginProperty, barcodeMargin);
+        return self;
+    }
+
+    public static T BarcodeMarginFmg<T>(this T self, Func<PropertyContext<int>, IPropertyBuilder<int>> configure)
+        where T : BarcodeGeneratorView
+    {
+        var context = new PropertyContext<int>(self, BarcodeGeneratorView.BarcodeMarginProperty);
+        configure(context).Build();
+        return self;
+    }
+
+    public static SettersContext<T> BarcodeMarginFmg<T>(this SettersContext<T> self,
+        int barcodeMargin)
+        where T : BarcodeGeneratorView
+    {
+        self.XamlSetters.Add(new Setter { Property = BarcodeGeneratorView.BarcodeMarginProperty, Value = barcodeMargin });
+        return self;
+    }
+
+    public static SettersContext<T> BarcodeMarginFmg<T>(this SettersContext<T> self, Func<PropertySettersContext<int>, IPropertySettersBuilder<int>> configure)
+        where T : BarcodeGeneratorView
+    {
+        var context = new PropertySettersContext<int>(self.XamlSetters, BarcodeGeneratorView.BarcodeMarginProperty);
+        configure(context).Build();
+        return self;
+    }
+}
---
+++ libs/FmgLib.MauiMarkup.ZXing/Extensions/CameraBarcodeReaderViewExtension.cs
@@ -0,0 +1,135 @@
+﻿using ZXing.Net.Maui;
+using ZXing.Net.Maui.Controls;
+
+namespace FmgLib.MauiMarkup.ZXing;
+
+public static partial class CameraBarcodeReaderViewExtension
+{
+    public static T OptionsFmg<T>(this T self,
+        BarcodeReaderOptions options)
+        where T : CameraBarcodeReaderView
+    {
+        self.SetValue(CameraBarcodeReaderView.OptionsProperty, options);
+        return self;
+    }
+
+    public static T OptionsFmg<T>(this T self, Func<PropertyContext<BarcodeReaderOptions>, IPropertyBuilder<BarcodeReaderOptions>> configure)
+        where T : CameraBarcodeReaderView
+    {
+        var context = new PropertyContext<BarcodeReaderOptions>(self, CameraBarcodeReaderView.OptionsProperty);
+        configure(context).Build();
+        return self;
+    }
+
+    public static SettersContext<T> OptionsFmg<T>(this SettersContext<T> self,
+        BarcodeReaderOptions options)
+        where T : CameraBarcodeReaderView
+    {
+        self.XamlSetters.Add(new Setter { Property = CameraBarcodeReaderView.OptionsProperty, Value = options });
+        return self;
+    }
+
+    public static SettersContext<T> OptionsFmg<T>(this SettersContext<T> self, Func<PropertySettersContext<BarcodeReaderOptions>, IPropertySettersBuilder<BarcodeReaderOptions>> configure)
+        where T : CameraBarcodeReaderView
+    {
+        var context = new PropertySettersContext<BarcodeReaderOptions>(self.XamlSetters, CameraBarcodeReaderView.OptionsProperty);
+        configure(context).Build();
+        return self;
+    }
+
+    public static T IsDetectingFmg<T>(this T self,
+        bool isDetecting)
+        where T : CameraBarcodeReaderView
+    {
+        self.SetValue(CameraBarcodeReaderView.IsDetectingProperty, isDetecting);
+        return self;
+    }
+
+    public static T IsDetectingFmg<T>(this T self, Func<PropertyContext<bool>, IPropertyBuilder<bool>> configure)
+        where T : CameraBarcodeReaderView
+    {
+        var context = new PropertyContext<bool>(self, CameraBarcodeReaderView.IsDetectingProperty);
+        configure(context).Build();
+        return self;
+    }
+
+    public static SettersContext<T> IsDetectingFmg<T>(this SettersContext<T> self,
+        bool isDetecting)
+        where T : CameraBarcodeReaderView
+    {
+        self.XamlSetters.Add(new Setter { Property = CameraBarcodeReaderView.IsDetectingProperty, Value = isDetecting });
+        return self;
+    }
+
+    public static SettersContext<T> IsDetectingFmg<T>(this SettersContext<T> self, Func<PropertySettersContext<bool>, IPropertySettersBuilder<bool>> configure)
+        where T : CameraBarcodeReaderView
+    {
+        var context = new PropertySettersContext<bool>(self.XamlSetters, CameraBarcodeReaderView.IsDetectingProperty);
+        configure(context).Build();
+        return self;
+    }
+
+    public static T IsTorchOnFmg<T>(this T self,
+        bool isTorchOn)
+        where T : CameraBarcodeReaderView
+    {
+        self.SetValue(CameraBarcodeReaderView.IsTorchOnProperty, isTorchOn);
+        return self;
+    }
+
+    public static T IsTorchOnFmg<T>(this T self, Func<PropertyContext<bool>, IPropertyBuilder<bool>> configure)
+        where T : CameraBarcodeReaderView
+    {
+        var context = new PropertyContext<bool>(self, CameraBarcodeReaderView.IsTorchOnProperty);
+        configure(context).Build();
+        return self;
+    }
+
+    public static SettersContext<T> IsTorchOnFmg<T>(this SettersContext<T> self,
+        bool isTorchOn)
+        where T : CameraBarcodeReaderView
+    {
+        self.XamlSetters.Add(new Setter { Property = CameraBarcodeReaderView.IsTorchOnProperty, Value = isTorchOn });
+        return self;
+    }
+
+    public static SettersContext<T> IsTorchOnFmg<T>(this SettersContext<T> self, Func<PropertySettersContext<bool>, IPropertySettersBuilder<bool>> configure)
+        where T : CameraBarcodeReaderView
+    {
+        var context = new PropertySettersContext<bool>(self.XamlSetters, CameraBarcodeReaderView.IsTorchOnProperty);
+        configure(context).Build();
+        return self;
+    }
+
+    public static T CameraLocationFmg<T>(this T self,
+        CameraLocation cameraLocation)
+        where T : CameraBarcodeReaderView
+    {
+        self.SetValue(CameraBarcodeReaderView.CameraLocationProperty, cameraLocation);
+        return self;
+    }
+
+    public static T CameraLocationFmg<T>(this T self, Func<PropertyContext<CameraLocation>, IPropertyBuilder<CameraLocation>> configure)
+        where T : CameraBarcodeReaderView
+    {
+        var context = new PropertyContext<CameraLocation>(self, CameraBarcodeReaderView.CameraLocationProperty);
+        configure(context).Build();
+        return self;
+    }
+
+    public static SettersContext<T> CameraLocationFmg<T>(this SettersContext<T> self,
+        CameraLocation cameraLocation)
+        where T : CameraBarcodeReaderView
+    {
+        self.XamlSetters.Add(new Setter { Property = CameraBarcodeReaderView.CameraLocationProperty, Value = cameraLocation });
+        return self;
+    }
+
+    public static SettersContext<T> CameraLocationFmg<T>(this SettersContext<T> self, Func<PropertySettersContext<CameraLocation>, IPropertySettersBuilder<CameraLocation>> configure)
+        where T : CameraBarcodeReaderView
+    {
+        var context = new PropertySettersContext<CameraLocation>(self.XamlSetters, CameraBarcodeReaderView.CameraLocationProperty);
+        configure(context).Build();
+        return self;
+    }
+}

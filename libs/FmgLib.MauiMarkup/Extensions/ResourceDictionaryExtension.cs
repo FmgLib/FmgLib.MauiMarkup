bc7ca19344297c1c0810e21e9d734889f85d06b2
Mustafa Gönültaş <gonultasmf@hotmail.com>
2024-05-08 22:58:56 +0300

    ResourceDictionaryExtension added. published(v8.2.3-prev1.0.1)

---
+++ libs/FmgLib.MauiMarkup/Extensions/ResourceDictionaryExtension.cs
@@ -0,0 +1,18 @@
+﻿namespace FmgLib.MauiMarkup;
+
+public static partial class ResourceDictionaryExtension
+{
+    public static ResourceDictionary Add(this ResourceDictionary dic, string key, object value)
+    {
+        dic.Add(key, value);
+
+        return dic;
+    }
+
+    public static ResourceDictionary Remove(this ResourceDictionary dic, string key)
+    {
+        dic.Remove(key);
+
+        return dic;
+    }
+}

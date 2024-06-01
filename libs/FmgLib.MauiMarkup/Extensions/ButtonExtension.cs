dc68b40e70a736a363bf1f8aaadc35ee242eb574
Mustafa Gönültaş <gonultasmf@hotmail.com>
2024-06-01 12:19:55 +0300

    Padding fixes.

--- libs/FmgLib.MauiMarkup/Extensions/ButtonExtension.cs
+++ libs/FmgLib.MauiMarkup/Extensions/ButtonExtension.cs
@@ -613,6 +613,24 @@ public static partial class ButtonExtension
         return self;
     }
 
+    public static T Padding<T>(this T self, double horizontalSize, double verticalSize) where T : Microsoft.Maui.Controls.Button
+    {
+        self.SetValue(Microsoft.Maui.Controls.Button.PaddingProperty, new Thickness(horizontalSize, verticalSize));
+        return self;
+    }
+
+    public static T Padding<T>(this T self, double left, double top, double right, double bottom) where T : Microsoft.Maui.Controls.Button
+    {
+        self.SetValue(Microsoft.Maui.Controls.Button.PaddingProperty, new Thickness(left, top, right, bottom));
+        return self;
+    }
+
+    public static T Padding<T>(this T self, object _ = null, double left = 0.0, double top = 0.0, double right = 0.0, double bottom = 0.0) where T : Microsoft.Maui.Controls.Button
+    {
+        self.SetValue(Microsoft.Maui.Controls.Button.PaddingProperty, new Thickness(left, top, right, bottom));
+        return self;
+    }
+
     public static T Padding<T>(this T self,
         Microsoft.Maui.Thickness padding)
         where T : Microsoft.Maui.Controls.Button
--- libs/FmgLib.MauiMarkup/Extensions/PageExtension.cs
+++ libs/FmgLib.MauiMarkup/Extensions/PageExtension.cs
@@ -100,6 +100,24 @@ public static partial class PageExtension
         return self;
     }
 
+    public static T Padding<T>(this T self, double horizontalSize, double verticalSize) where T : Page
+    {
+        self.SetValue(Page.PaddingProperty, new Thickness(horizontalSize, verticalSize));
+        return self;
+    }
+
+    public static T Padding<T>(this T self, double left, double top, double right, double bottom) where T : Page
+    {
+        self.SetValue(Page.PaddingProperty, new Thickness(left, top, right, bottom));
+        return self;
+    }
+
+    public static T Padding<T>(this T self, object _ = null, double left = 0.0, double top = 0.0, double right = 0.0, double bottom = 0.0) where T : Page
+    {
+        self.SetValue(Page.PaddingProperty, new Thickness(left, top, right, bottom));
+        return self;
+    }
+
     public static T Padding<T>(this T self,
         Thickness padding)
         where T : Page

b32ed02fc73185b462656444711bf2423c1369fe
Mustafa Gönültaş <gonultasmf@hotmail.com>
2024-08-05 22:00:47 +0300

    TextAlign fixes.

--- libs/FmgLib.MauiMarkup/Extensions/EditorExtension.cs
+++ libs/FmgLib.MauiMarkup/Extensions/EditorExtension.cs
@@ -346,90 +346,6 @@ public static partial class EditorExtension
     }
 
 
-    public static T TextCenterHorizontal<T>(this T self)
-        where T : Editor
-    {
-        self.SetValue(Editor.HorizontalTextAlignmentProperty, TextAlignment.Center);
-        return self;
-    }
-
-    public static T TextCenterVertical<T>(this T self)
-        where T : Editor
-    {
-        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.Center);
-        return self;
-    }
-
-    public static T TextCenter<T>(this T self)
-        where T : Editor
-    {
-        self.SetValue(Editor.HorizontalTextAlignmentProperty, TextAlignment.Center);
-        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.Center);
-        return self;
-    }
-
-    public static T TextTop<T>(this T self)
-        where T : Editor
-    {
-        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.Start);
-        return self;
-    }
-
-    public static T TextBottom<T>(this T self)
-        where T : Editor, Microsoft.Maui.ITextAlignment
-    {
-        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.End);
-        return self;
-    }
-
-    public static T TextTopStart<T>(this T self)
-        where T : Editor
-    {
-        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.Start);
-        self.SetValue(Editor.HorizontalTextAlignmentProperty, TextAlignment.Start);
-        return self;
-    }
-
-    public static T TextBottomStart<T>(this T self)
-        where T : Editor
-    {
-        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.End);
-        self.SetValue(Editor.HorizontalTextAlignmentProperty, TextAlignment.Start);
-        return self;
-    }
-
-    public static T TextTopCenterH<T>(this T self)
-        where T : Editor
-    {
-        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.Start);
-        self.SetValue(Editor.HorizontalTextAlignmentProperty, TextAlignment.Center);
-        return self;
-    }
-
-    public static T TextBottomCenterH<T>(this T self)
-        where T : Editor
-    {
-        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.End);
-        self.SetValue(Editor.HorizontalTextAlignmentProperty, TextAlignment.Center);
-        return self;
-    }
-
-    public static T TextCenterVEnd<T>(this T self)
-        where T : Editor
-    {
-        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.Start);
-        self.SetValue(Editor.HorizontalTextAlignmentProperty, TextAlignment.End);
-        return self;
-    }
-
-    public static T TextCenterVStart<T>(this T self)
-        where T : Editor
-    {
-        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.End);
-        self.SetValue(Editor.HorizontalTextAlignmentProperty, TextAlignment.End);
-        return self;
-    }
-
     public static T AlignText<T>(this T self, TextAlignment vertical, TextAlignment horizontal)
         where T : Editor
     {
@@ -446,7 +362,175 @@ public static partial class EditorExtension
         return self;
     }
 
-    public static T TextTopEnd<T>(this T self)
+    public static T TextCenterHorizontal<T>(this T self)
+        where T : Editor
+    {
+        self.SetValue(Editor.HorizontalTextAlignmentProperty, TextAlignment.Center);
+        return self;
+    }
+
+    public static SettersContext<T> TextCenterHorizontal<T>(this SettersContext<T> self)
+        where T : Editor
+    {
+        self.XamlSetters.Add(new Setter { Property = Editor.HorizontalTextAlignmentProperty, Value = TextAlignment.Center });
+        return self;
+    }
+
+    public static T TextCenterVertical<T>(this T self)
+        where T : Editor
+    {
+        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.Center);
+        return self;
+    }
+
+    public static SettersContext<T> TextCenterVertical<T>(this SettersContext<T> self)
+        where T : Editor
+    {
+        self.XamlSetters.Add(new Setter { Property = Editor.VerticalTextAlignmentProperty, Value = TextAlignment.Center });
+        return self;
+    }
+
+    public static T TextCenter<T>(this T self)
+        where T : Editor
+    {
+        self.SetValue(Editor.HorizontalTextAlignmentProperty, TextAlignment.Center);
+        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.Center);
+        return self;
+    }
+
+    public static SettersContext<T> TextCenter<T>(this SettersContext<T> self)
+        where T : Editor
+    {
+        self.XamlSetters.Add(new Setter { Property = Editor.VerticalTextAlignmentProperty, Value = TextAlignment.Center });
+        self.XamlSetters.Add(new Setter { Property = Editor.HorizontalTextAlignmentProperty, Value = TextAlignment.Center });
+        return self;
+    }
+
+    public static T TextTop<T>(this T self)
+        where T : Editor
+    {
+        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.Start);
+        return self;
+    }
+
+    public static SettersContext<T> TextTop<T>(this SettersContext<T> self)
+        where T : Editor
+    {
+        self.XamlSetters.Add(new Setter { Property = Editor.VerticalTextAlignmentProperty, Value = TextAlignment.Start });
+        return self;
+    }
+
+    public static T TextBottom<T>(this T self)
+        where T : Editor
+    {
+        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.End);
+        return self;
+    }
+
+    public static SettersContext<T> TextBottom<T>(this SettersContext<T> self)
+        where T : Editor
+    {
+        self.XamlSetters.Add(new Setter { Property = Editor.VerticalTextAlignmentProperty, Value = TextAlignment.End });
+        return self;
+    }
+
+    public static T TextTopLeft<T>(this T self)
+        where T : Editor
+    {
+        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.Start);
+        self.SetValue(Editor.HorizontalTextAlignmentProperty, TextAlignment.Start);
+        return self;
+    }
+
+    public static SettersContext<T> TextTopLeft<T>(this SettersContext<T> self)
+        where T : Editor
+    {
+        self.XamlSetters.Add(new Setter { Property = Editor.VerticalTextAlignmentProperty, Value = TextAlignment.Start });
+        self.XamlSetters.Add(new Setter { Property = Editor.HorizontalTextAlignmentProperty, Value = TextAlignment.Start });
+        return self;
+    }
+
+    public static T TextBottomLeft<T>(this T self)
+        where T : Editor
+    {
+        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.End);
+        self.SetValue(Editor.HorizontalTextAlignmentProperty, TextAlignment.Start);
+        return self;
+    }
+
+    public static SettersContext<T> TextBottomLeft<T>(this SettersContext<T> self)
+        where T : Editor
+    {
+        self.XamlSetters.Add(new Setter { Property = Editor.VerticalTextAlignmentProperty, Value = TextAlignment.End });
+        self.XamlSetters.Add(new Setter { Property = Editor.HorizontalTextAlignmentProperty, Value = TextAlignment.Start });
+        return self;
+    }
+
+    public static T TextTopCenter<T>(this T self)
+        where T : Editor
+    {
+        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.Start);
+        self.SetValue(Editor.HorizontalTextAlignmentProperty, TextAlignment.Center);
+        return self;
+    }
+
+    public static SettersContext<T> TextTopCenter<T>(this SettersContext<T> self)
+        where T : Editor
+    {
+        self.XamlSetters.Add(new Setter { Property = Editor.VerticalTextAlignmentProperty, Value = TextAlignment.Start });
+        self.XamlSetters.Add(new Setter { Property = Editor.HorizontalTextAlignmentProperty, Value = TextAlignment.Center });
+        return self;
+    }
+
+    public static T TextBottomCenter<T>(this T self)
+        where T : Editor
+    {
+        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.End);
+        self.SetValue(Editor.HorizontalTextAlignmentProperty, TextAlignment.Center);
+        return self;
+    }
+
+    public static SettersContext<T> TextBottomCenter<T>(this SettersContext<T> self)
+        where T : Editor
+    {
+        self.XamlSetters.Add(new Setter { Property = Editor.VerticalTextAlignmentProperty, Value = TextAlignment.End });
+        self.XamlSetters.Add(new Setter { Property = Editor.HorizontalTextAlignmentProperty, Value = TextAlignment.Center });
+        return self;
+    }
+
+    public static T TextCenterRight<T>(this T self)
+        where T : Editor
+    {
+        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.Center);
+        self.SetValue(Editor.HorizontalTextAlignmentProperty, TextAlignment.End);
+        return self;
+    }
+
+    public static SettersContext<T> TextCenterRight<T>(this SettersContext<T> self)
+        where T : Editor
+    {
+        self.XamlSetters.Add(new Setter { Property = Editor.VerticalTextAlignmentProperty, Value = TextAlignment.Center });
+        self.XamlSetters.Add(new Setter { Property = Editor.HorizontalTextAlignmentProperty, Value = TextAlignment.End });
+        return self;
+    }
+
+    public static T TextCenterLeft<T>(this T self)
+        where T : Editor
+    {
+        self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.Center);
+        self.SetValue(Editor.HorizontalTextAlignmentProperty, TextAlignment.Start);
+        return self;
+    }
+
+    public static SettersContext<T> TextCenterLeft<T>(this SettersContext<T> self)
+        where T : Editor
+    {
+        self.XamlSetters.Add(new Setter { Property = Editor.VerticalTextAlignmentProperty, Value = TextAlignment.Center });
+        self.XamlSetters.Add(new Setter { Property = Editor.HorizontalTextAlignmentProperty, Value = TextAlignment.Start });
+        return self;
+    }
+
+    public static T TextTopRight<T>(this T self)
         where T : Editor
     {
         self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.Start);
@@ -454,7 +538,15 @@ public static partial class EditorExtension
         return self;
     }
 
-    public static T TextBottomEnd<T>(this T self)
+    public static SettersContext<T> TextTopRight<T>(this SettersContext<T> self)
+        where T : Editor
+    {
+        self.XamlSetters.Add(new Setter { Property = Editor.VerticalTextAlignmentProperty, Value = TextAlignment.Start });
+        self.XamlSetters.Add(new Setter { Property = Editor.HorizontalTextAlignmentProperty, Value = TextAlignment.End });
+        return self;
+    }
+
+    public static T TextBottomRight<T>(this T self)
         where T : Editor
     {
         self.SetValue(Editor.VerticalTextAlignmentProperty, TextAlignment.End);
@@ -462,18 +554,39 @@ public static partial class EditorExtension
         return self;
     }
 
-    public static T TextStart<T>(this T self)
+    public static SettersContext<T> TextBottomRight<T>(this SettersContext<T> self)
+        where T : Editor
+    {
+        self.XamlSetters.Add(new Setter { Property = Editor.VerticalTextAlignmentProperty, Value = TextAlignment.End });
+        self.XamlSetters.Add(new Setter { Property = Editor.HorizontalTextAlignmentProperty, Value = TextAlignment.End });
+        return self;
+    }
+
+    public static T TextLeft<T>(this T self)
         where T : Editor
     {
         self.SetValue(Editor.HorizontalTextAlignmentProperty, TextAlignment.Start);
         return self;
     }
 
-    public static T TextEnd<T>(this T self)
+    public static SettersContext<T> TextLeft<T>(this SettersContext<T> self)
+        where T : Editor
+    {
+        self.XamlSetters.Add(new Setter { Property = Editor.HorizontalTextAlignmentProperty, Value = TextAlignment.Start });
+        return self;
+    }
+
+    public static T TextRight<T>(this T self)
         where T : Editor
     {
         self.SetValue(Editor.HorizontalTextAlignmentProperty, TextAlignment.End);
         return self;
     }
 
+    public static SettersContext<T> TextRight<T>(this SettersContext<T> self)
+        where T : Editor
+    {
+        self.XamlSetters.Add(new Setter { Property = Editor.HorizontalTextAlignmentProperty, Value = TextAlignment.End });
+        return self;
+    }
 }

8849509665a7a6d2ec2c484c13acad733bfccf60
Mustafa Gönültaş <gonultasmf@hotmail.com>
2024-08-15 22:25:44 +0300

    Bindablelayout extension fixes.

--- libs/FmgLib.MauiMarkup/Extensions/BindableLayoutExtensions.cs
+++ libs/FmgLib.MauiMarkup/Extensions/BindableLayoutExtensions.cs
@@ -7,54 +7,54 @@ public static class BindableLayoutExtensions
 {
     public static T FlexAlignSelf<T>(this T view, FlexAlignSelf value) where T : View
     {
-        FlexLayout.SetAlignSelf((BindableObject)view, value);
+        FlexLayout.SetAlignSelf(view, value);
         return view;
     }
 
     public static T FlexBasis<T>(this T view, FlexBasis value) where T : View
     {
-        FlexLayout.SetBasis((BindableObject)view, value);
+        FlexLayout.SetBasis(view, value);
         return view;
     }
 
     public static T FlexOrder<T>(this T view, int value) where T : View
     {
-        FlexLayout.SetOrder((BindableObject)view, value);
+        FlexLayout.SetOrder(view, value);
         return view;
     }
 
     public static T FlexGrow<T>(this T view, float value) where T : View
     {
-        FlexLayout.SetGrow((BindableObject)view, value);
+        FlexLayout.SetGrow(view, value);
         return view;
     }
 
     public static T FlexShrink<T>(this T view, float value) where T : View
     {
-        FlexLayout.SetShrink((BindableObject)view, value);
+        FlexLayout.SetShrink(view, value);
         return view;
     }
 
-    public static T EmptyViewTemplates<T>(this T layout, DataTemplate template) where T : BindableObject, Microsoft.Maui.ILayout
+    public static T BindableLayoutEmptyViewTemplate<T>(this T layout, DataTemplate template) where T : BindableObject, Microsoft.Maui.ILayout
     {
         BindableLayout.SetEmptyViewTemplate(layout, template);
         return layout;
     }
 
-    public static T ItemsSources<T>(this T layout, IEnumerable source) where T : BindableObject, Microsoft.Maui.ILayout
+    public static T BindableLayoutItemsSource<T>(this T layout, IEnumerable source) where T : BindableObject, Microsoft.Maui.ILayout
     {
         BindableLayout.SetItemsSource(layout, source);
         return layout;
     }
 
-    public static T ItemsSources<T>(this T layout, Func<PropertyContext<IEnumerable>, IPropertyBuilder<IEnumerable>> configure) where T : BindableObject, Microsoft.Maui.ILayout
+    public static T BindableLayoutItemsSource<T>(this T layout, Func<PropertyContext<IEnumerable>, IPropertyBuilder<IEnumerable>> configure) where T : BindableObject, Microsoft.Maui.ILayout
     {
         var context = new PropertyContext<IEnumerable>(layout, BindableLayout.ItemsSourceProperty);
         configure(context).Build();
         return layout;
     }
 
-    public static T ItemTemplates<T>(this T layout, DataTemplate template) where T : BindableObject, Microsoft.Maui.ILayout
+    public static T BindableLayoutItemTemplate<T>(this T layout, DataTemplate template) where T : BindableObject, Microsoft.Maui.ILayout
     {
         BindableLayout.SetItemTemplate(layout, template);
         return layout;

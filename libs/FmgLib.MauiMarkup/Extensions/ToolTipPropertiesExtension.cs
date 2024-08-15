d46eabd87d01bff7514ad9963a79236fb3360d38
Mustafa Gönültaş <gonultasmf@hotmail.com>
2024-08-15 23:56:02 +0300

    ToolTipPropertiesExtension added. BindableLayoutEmptyView and BindableItemTemplateSelector added

---
+++ libs/FmgLib.MauiMarkup/Extensions/ToolTipPropertiesExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace FmgLib.MauiMarkup;
+
+public static partial class ToolTipPropertiesExtension
+{
+    public static T ToolTipPropertiesText<T>(this T view, string text) where T : View
+    {
+        ToolTipProperties.SetText(view, text);
+
+        return view;
+    }
+}

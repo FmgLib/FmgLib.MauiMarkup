6fa3d27829a3f11609b3adcb36e6c3eba7eff540
Mustafa Gönültaş <gonultasmf@hotmail.com>
2024-08-30 09:55:35 +0300

    contentview template added.

---
+++ sample/MyOrderApp/NewContent1.cs
@@ -0,0 +1,15 @@
+namespace MyOrderApp;
+
+public class NewContent1 : ContentView
+{
+	public NewContent1()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}

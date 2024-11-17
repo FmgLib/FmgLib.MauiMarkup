753da52b7d03d6cd4af68c889711cec43fa2a392
Mustafa Gönültaş <gonultasmf@hotmail.com>
2024-11-17 19:59:50 +0300

    #149 Animate extension method fix.

--- src/FmgLib.MauiMarkup.Generator/Extensions/ExtensionGenerator.AnimateTo.cs
+++ src/FmgLib.MauiMarkup.Generator/Extensions/ExtensionGenerator.AnimateTo.cs
@@ -13,12 +13,13 @@ public partial class ExtensionGenerator
     public static Task<bool> Animate{info.propertyName}To<T>(this T self, {info.propertyTypeName} value, uint length = 250, Easing? easing = null)
         where T : {info.MainSymbolName}");
 
+        var callBackSetValue = info.IsBindableProperty ? $"self.SetValue({info.BindablePropertyName}, actValue);" : $"self.{info.propertyName} = actValue;";
 
         builder.Append($@"
     {{
         {info.propertyTypeName} fromValue = self.{info.propertyName};
         var transform = (double t) => Transformations.{transformationName}(fromValue, value, t);
-        var callback = ({info.propertyTypeName} actValue) => {{ self.{info.propertyName} = actValue; }};
+        var callback = ({info.propertyTypeName} actValue) => {{ {callBackSetValue} }};
         return Transformations.AnimateAsync<{info.propertyTypeName}>(self, ""Animate{info.propertyName}To"", transform, callback, length, easing);
     }}
     ");

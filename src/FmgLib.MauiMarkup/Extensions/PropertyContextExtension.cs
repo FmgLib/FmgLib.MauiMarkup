12075163fbebbf51e921924906a3233b59a3d4cc
gonultasmf <gonultasmf@hotmail.com>
2025-03-31 16:31:20 +0300

    Refactor PropertyContext and PropertySettersContext extension methods to support generic source and property types

--- src/FmgLib.MauiMarkup/Extensions/PropertyContextExtension.cs
+++ src/FmgLib.MauiMarkup/Extensions/PropertyContextExtension.cs
@@ -3,87 +3,87 @@
 public static class PropertyContextExtension
 {
 
-    public static PropertyBindingBuilder<T> Path<T>(this PropertyContext<T> self, string path)
-        => new PropertyBindingBuilder<T>(self).Path(path);
+    public static PropertyBindingBuilder<TSource, TProperty> Path<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, string path)
+        => new PropertyBindingBuilder<TSource, TProperty>(self).Path(path);
     
-    public static PropertyBindingBuilder<T> Compiled<T>(this PropertyContext<T> self, Func<object, T> getter)
-        => new PropertyBindingBuilder<T>(self).Compiled(getter);
+    public static PropertyBindingBuilder<TSource, TProperty> Compiled<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, Func<TSource, TProperty> getter)
+        => new PropertyBindingBuilder<TSource, TProperty>(self).Compiled(getter);
 
-    public static PropertyMultiBindingBuilder<T> Bindings<T>(this PropertyContext<T> self, params BindingBase[] bindings)
-        => new PropertyMultiBindingBuilder<T>(self).Bindings(bindings);
+    public static PropertyMultiBindingBuilder<TSource, TProperty> Bindings<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, params BindingBase[] bindings)
+        => new PropertyMultiBindingBuilder<TSource, TProperty>(self).Bindings(bindings);
 
-    public static PropertyIdiomBuilder<T> OnPhone<T>(this PropertyContext<T> self, T value)
-        => new PropertyIdiomBuilder<T>(self).OnPhone(value);
+    public static PropertyIdiomBuilder<TSource, TProperty> OnPhone<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, TProperty value)
+        => new PropertyIdiomBuilder<TSource, TProperty>(self).OnPhone(value);
 
-    public static PropertyIdiomBuilder<T> OnPhone<T>(this PropertyContext<T> self, Func<PropertyContext<T>, IPropertyBuilder<T>> configure)
-        => new PropertyIdiomBuilder<T>(self).OnPhone(configure);
+    public static PropertyIdiomBuilder<TSource, TProperty> OnPhone<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, Func<PropertyContext<TSource, TProperty>, IPropertyBuilder<TSource, TProperty>> configure)
+        => new PropertyIdiomBuilder<TSource, TProperty>(self).OnPhone(configure);
 
-    public static PropertyIdiomBuilder<T> OnTablet<T>(this PropertyContext<T> self, T value)
-        => new PropertyIdiomBuilder<T>(self).OnTablet(value);
+    public static PropertyIdiomBuilder<TSource, TProperty> OnTablet<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, TProperty value)
+        => new PropertyIdiomBuilder<TSource, TProperty>(self).OnTablet(value);
 
-    public static PropertyIdiomBuilder<T> OnTablet<T>(this PropertyContext<T> self, Func<PropertyContext<T>, IPropertyBuilder<T>> configure)
-        => new PropertyIdiomBuilder<T>(self).OnTablet(configure);
+    public static PropertyIdiomBuilder<TSource, TProperty> OnTablet<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, Func<PropertyContext<TSource, TProperty>, IPropertyBuilder<TSource, TProperty>> configure)
+        => new PropertyIdiomBuilder<TSource, TProperty>(self).OnTablet(configure);
 
-    public static PropertyIdiomBuilder<T> OnDesktop<T>(this PropertyContext<T> self, T value)
-        => new PropertyIdiomBuilder<T>(self).OnDesktop(value);
+    public static PropertyIdiomBuilder<TSource, TProperty> OnDesktop<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, TProperty value)
+        => new PropertyIdiomBuilder<TSource, TProperty>(self).OnDesktop(value);
 
-    public static PropertyIdiomBuilder<T> OnDesktop<T>(this PropertyContext<T> self, Func<PropertyContext<T>, IPropertyBuilder<T>> configure)
-        => new PropertyIdiomBuilder<T>(self).OnDesktop(configure);
+    public static PropertyIdiomBuilder<TSource, TProperty> OnDesktop<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, Func<PropertyContext<TSource, TProperty>, IPropertyBuilder<TSource, TProperty>> configure)
+        => new PropertyIdiomBuilder<TSource, TProperty>(self).OnDesktop(configure);
 
-    public static PropertyIdiomBuilder<T> OnTV<T>(this PropertyContext<T> self, T value)
-        => new PropertyIdiomBuilder<T>(self).OnTV(value);
+    public static PropertyIdiomBuilder<TSource, TProperty> OnTV<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, TProperty value)
+        => new PropertyIdiomBuilder<TSource, TProperty>(self).OnTV(value);
 
-    public static PropertyIdiomBuilder<T> OnTV<T>(this PropertyContext<T> self, Func<PropertyContext<T>, IPropertyBuilder<T>> configure)
-        => new PropertyIdiomBuilder<T>(self).OnTV(configure);
+    public static PropertyIdiomBuilder<TSource, TProperty> OnTV<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, Func<PropertyContext<TSource, TProperty>, IPropertyBuilder<TSource, TProperty>> configure)
+        => new PropertyIdiomBuilder<TSource, TProperty>(self).OnTV(configure);
 
-    public static PropertyIdiomBuilder<T> OnWatch<T>(this PropertyContext<T> self, T value)
-        => new PropertyIdiomBuilder<T>(self).OnWatch(value);
+    public static PropertyIdiomBuilder<TSource, TProperty> OnWatch<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, TProperty value)
+        => new PropertyIdiomBuilder<TSource, TProperty>(self).OnWatch(value);
 
-    public static PropertyIdiomBuilder<T> OnWatch<T>(this PropertyContext<T> self, Func<PropertyContext<T>, IPropertyBuilder<T>> configure)
-        => new PropertyIdiomBuilder<T>(self).OnWatch(configure);
+    public static PropertyIdiomBuilder<TSource, TProperty> OnWatch<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, Func<PropertyContext<TSource, TProperty>, IPropertyBuilder<TSource, TProperty>> configure)
+        => new PropertyIdiomBuilder<TSource, TProperty>(self).OnWatch(configure);
 
-    public static PropertyPlatformBuilder<T> OnMacCatalyst<T>(this PropertyContext<T> self, T value)
-        => new PropertyPlatformBuilder<T>(self).OnMacCatalyst(value);
+    public static PropertyPlatformBuilder<TSource, TProperty> OnMacCatalyst<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, TProperty value)
+        => new PropertyPlatformBuilder<TSource, TProperty>(self).OnMacCatalyst(value);
 
-    public static PropertyPlatformBuilder<T> OnMacCatalyst<T>(this PropertyContext<T> self, Func<PropertyContext<T>, IPropertyBuilder<T>> configure)
-        => new PropertyPlatformBuilder<T>(self).OnMacCatalyst(configure);
+    public static PropertyPlatformBuilder<TSource, TProperty> OnMacCatalyst<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, Func<PropertyContext<TSource, TProperty>, IPropertyBuilder<TSource, TProperty>> configure)
+        => new PropertyPlatformBuilder<TSource, TProperty>(self).OnMacCatalyst(configure);
 
-    public static PropertyPlatformBuilder<T> OniOS<T>(this PropertyContext<T> self, T value)
-        => new PropertyPlatformBuilder<T>(self).OniOS(value);
+    public static PropertyPlatformBuilder<TSource, TProperty> OniOS<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, TProperty value)
+        => new PropertyPlatformBuilder<TSource, TProperty>(self).OniOS(value);
 
-    public static PropertyPlatformBuilder<T> OniOS<T>(this PropertyContext<T> self, Func<PropertyContext<T>, IPropertyBuilder<T>> configure)
-        => new PropertyPlatformBuilder<T>(self).OniOS(configure);
+    public static PropertyPlatformBuilder<TSource, TProperty> OniOS<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, Func<PropertyContext<TSource, TProperty>, IPropertyBuilder<TSource, TProperty>> configure)
+        => new PropertyPlatformBuilder<TSource, TProperty>(self).OniOS(configure);
 
-    public static PropertyPlatformBuilder<T> OnAndroid<T>(this PropertyContext<T> self, T value)
-        => new PropertyPlatformBuilder<T>(self).OnAndroid(value);
+    public static PropertyPlatformBuilder<TSource, TProperty> OnAndroid<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, TProperty value)
+        => new PropertyPlatformBuilder<TSource, TProperty>(self).OnAndroid(value);
 
-    public static PropertyPlatformBuilder<T> OnAndroid<T>(this PropertyContext<T> self, Func<PropertyContext<T>, IPropertyBuilder<T>> configure)
-        => new PropertyPlatformBuilder<T>(self).OnAndroid(configure);
+    public static PropertyPlatformBuilder<TSource, TProperty> OnAndroid<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, Func<PropertyContext<TSource, TProperty>, IPropertyBuilder<TSource, TProperty>> configure)
+        => new PropertyPlatformBuilder<TSource, TProperty>(self).OnAndroid(configure);
 
-    public static PropertyPlatformBuilder<T> OnWinUI<T>(this PropertyContext<T> self, T value)
-        => new PropertyPlatformBuilder<T>(self).OnWinUI(value);
+    public static PropertyPlatformBuilder<TSource, TProperty> OnWinUI<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, TProperty value)
+        => new PropertyPlatformBuilder<TSource, TProperty>(self).OnWinUI(value);
 
-    public static PropertyPlatformBuilder<T> OnWinUI<T>(this PropertyContext<T> self, Func<PropertyContext<T>, IPropertyBuilder<T>> configure)
-        => new PropertyPlatformBuilder<T>(self).OnWinUI(configure);
+    public static PropertyPlatformBuilder<TSource, TProperty> OnWinUI<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, Func<PropertyContext<TSource, TProperty>, IPropertyBuilder<TSource, TProperty>> configure)
+        => new PropertyPlatformBuilder<TSource, TProperty>(self).OnWinUI(configure);
 
-    public static PropertyPlatformBuilder<T> OnTizen<T>(this PropertyContext<T> self, T value)
-        => new PropertyPlatformBuilder<T>(self).OnTizen(value);
+    public static PropertyPlatformBuilder<TSource, TProperty> OnTizen<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, TProperty value)
+        => new PropertyPlatformBuilder<TSource, TProperty>(self).OnTizen(value);
 
-    public static PropertyPlatformBuilder<T> OnTizen<T>(this PropertyContext<T> self, Func<PropertyContext<T>, IPropertyBuilder<T>> configure)
-        => new PropertyPlatformBuilder<T>(self).OnTizen(configure);
+    public static PropertyPlatformBuilder<TSource, TProperty> OnTizen<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, Func<PropertyContext<TSource, TProperty>, IPropertyBuilder<TSource, TProperty>> configure)
+        => new PropertyPlatformBuilder<TSource, TProperty>(self).OnTizen(configure);
 
-    public static PropertyThemeBuilder<T> OnLight<T>(this PropertyContext<T> self, T value)
-        => new PropertyThemeBuilder<T>(self).OnLight(value);
+    public static PropertyThemeBuilder<TSource, TProperty> OnLight<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, TProperty value)
+        => new PropertyThemeBuilder<TSource, TProperty>(self).OnLight(value);
 
-    public static PropertyThemeBuilder<T> OnLight<T>(this PropertyContext<T> self, Func<PropertyContext<T>, IPropertyBuilder<T>> configure)
-        => new PropertyThemeBuilder<T>(self).OnLight(configure);
+    public static PropertyThemeBuilder<TSource, TProperty> OnLight<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, Func<PropertyContext<TSource, TProperty>, IPropertyBuilder<TSource, TProperty>> configure)
+        => new PropertyThemeBuilder<TSource, TProperty>(self).OnLight(configure);
 
-    public static PropertyThemeBuilder<T> OnDark<T>(this PropertyContext<T> self, T value)
-        => new PropertyThemeBuilder<T>(self).OnDark(value);
+    public static PropertyThemeBuilder<TSource, TProperty> OnDark<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, TProperty value)
+        => new PropertyThemeBuilder<TSource, TProperty>(self).OnDark(value);
 
-    public static PropertyThemeBuilder<T> OnDark<T>(this PropertyContext<T> self, Func<PropertyContext<T>, IPropertyBuilder<T>> configure)
-        => new PropertyThemeBuilder<T>(self).OnDark(configure);
+    public static PropertyThemeBuilder<TSource, TProperty> OnDark<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, Func<PropertyContext<TSource, TProperty>, IPropertyBuilder<TSource, TProperty>> configure)
+        => new PropertyThemeBuilder<TSource, TProperty>(self).OnDark(configure);
 
-    public static PropertyDynamicResourcesBuilder<T> DynamicResource<T>(this PropertyContext<T> self, string key)
-        => new PropertyDynamicResourcesBuilder<T>(self).DynamicResource(key);
+    public static PropertyDynamicResourcesBuilder<TSource, TProperty> DynamicResource<TSource, TProperty>(this PropertyContext<TSource, TProperty> self, string key)
+        => new PropertyDynamicResourcesBuilder<TSource, TProperty>(self).DynamicResource(key);
 }
--- src/FmgLib.MauiMarkup/Extensions/PropertySettersContextExtension.cs
+++ src/FmgLib.MauiMarkup/Extensions/PropertySettersContextExtension.cs
@@ -2,87 +2,87 @@
 
 public static class PropertySettersContextExtension
 {
-    public static PropertySettersBindingBuilder<T> Path<T>(this PropertySettersContext<T> self, string path)
-        => new PropertySettersBindingBuilder<T>(self).Path(path);
+    public static PropertySettersBindingBuilder<TSource, TProperty> Path<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, string path)
+        => new PropertySettersBindingBuilder<TSource, TProperty>(self).Path(path);
     
-    public static PropertySettersBindingBuilder<T> Compiled<T>(this PropertySettersContext<T> self, Func<object, T> getter)
-        => new PropertySettersBindingBuilder<T>(self).Compiled(getter);
+    public static PropertySettersBindingBuilder<TSource, TProperty> Compiled<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, Func<TSource, TProperty> getter)
+        => new PropertySettersBindingBuilder<TSource, TProperty>(self).Compiled(getter);
 
-    public static PropertySettersMultiBindingBuilder<T> Bindings<T>(this PropertySettersContext<T> self, params BindingBase[] bindings)
-        => new PropertySettersMultiBindingBuilder<T>(self).Bindings(bindings);
+    public static PropertySettersMultiBindingBuilder<TSource, TProperty> Bindings<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, params BindingBase[] bindings)
+        => new PropertySettersMultiBindingBuilder<TSource, TProperty>(self).Bindings(bindings);
 
-    public static PropertySettersIdiomBuilder<T> OnPhone<T>(this PropertySettersContext<T> self, T value)
-        => new PropertySettersIdiomBuilder<T>(self).OnPhone(value);
+    public static PropertySettersIdiomBuilder<TSource, TProperty> OnPhone<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, TProperty value)
+        => new PropertySettersIdiomBuilder<TSource, TProperty>(self).OnPhone(value);
 
-    public static PropertySettersIdiomBuilder<T> OnPhone<T>(this PropertySettersContext<T> self, Func<PropertySettersContext<T>, IPropertySettersBuilder<T>> configure)
-        => new PropertySettersIdiomBuilder<T>(self).OnPhone(configure);
+    public static PropertySettersIdiomBuilder<TSource, TProperty> OnPhone<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, Func<PropertySettersContext<TSource, TProperty>, IPropertySettersBuilder<TSource, TProperty>> configure)
+        => new PropertySettersIdiomBuilder<TSource, TProperty>(self).OnPhone(configure);
 
-    public static PropertySettersIdiomBuilder<T> OnTablet<T>(this PropertySettersContext<T> self, T value)
-        => new PropertySettersIdiomBuilder<T>(self).OnTablet(value);
+    public static PropertySettersIdiomBuilder<TSource, TProperty> OnTablet<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, TProperty value)
+        => new PropertySettersIdiomBuilder<TSource, TProperty>(self).OnTablet(value);
 
-    public static PropertySettersIdiomBuilder<T> OnTablet<T>(this PropertySettersContext<T> self, Func<PropertySettersContext<T>, IPropertySettersBuilder<T>> configure)
-        => new PropertySettersIdiomBuilder<T>(self).OnTablet(configure);
+    public static PropertySettersIdiomBuilder<TSource, TProperty> OnTablet<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, Func<PropertySettersContext<TSource, TProperty>, IPropertySettersBuilder<TSource, TProperty>> configure)
+        => new PropertySettersIdiomBuilder<TSource, TProperty>(self).OnTablet(configure);
 
-    public static PropertySettersIdiomBuilder<T> OnDesktop<T>(this PropertySettersContext<T> self, T value)
-        => new PropertySettersIdiomBuilder<T>(self).OnDesktop(value);
+    public static PropertySettersIdiomBuilder<TSource, TProperty> OnDesktop<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, TProperty value)
+        => new PropertySettersIdiomBuilder<TSource, TProperty>(self).OnDesktop(value);
 
-    public static PropertySettersIdiomBuilder<T> OnDesktop<T>(this PropertySettersContext<T> self, Func<PropertySettersContext<T>, IPropertySettersBuilder<T>> configure)
-        => new PropertySettersIdiomBuilder<T>(self).OnDesktop(configure);
+    public static PropertySettersIdiomBuilder<TSource, TProperty> OnDesktop<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, Func<PropertySettersContext<TSource, TProperty>, IPropertySettersBuilder<TSource, TProperty>> configure)
+        => new PropertySettersIdiomBuilder<TSource, TProperty>(self).OnDesktop(configure);
 
-    public static PropertySettersIdiomBuilder<T> OnTV<T>(this PropertySettersContext<T> self, T value)
-        => new PropertySettersIdiomBuilder<T>(self).OnTV(value);
+    public static PropertySettersIdiomBuilder<TSource, TProperty> OnTV<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, TProperty value)
+        => new PropertySettersIdiomBuilder<TSource, TProperty>(self).OnTV(value);
 
-    public static PropertySettersIdiomBuilder<T> OnTV<T>(this PropertySettersContext<T> self, Func<PropertySettersContext<T>, IPropertySettersBuilder<T>> configure)
-        => new PropertySettersIdiomBuilder<T>(self).OnTV(configure);
+    public static PropertySettersIdiomBuilder<TSource, TProperty> OnTV<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, Func<PropertySettersContext<TSource, TProperty>, IPropertySettersBuilder<TSource, TProperty>> configure)
+        => new PropertySettersIdiomBuilder<TSource, TProperty>(self).OnTV(configure);
 
-    public static PropertySettersIdiomBuilder<T> OnWatch<T>(this PropertySettersContext<T> self, T value)
-        => new PropertySettersIdiomBuilder<T>(self).OnWatch(value);
+    public static PropertySettersIdiomBuilder<TSource, TProperty> OnWatch<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, TProperty value)
+        => new PropertySettersIdiomBuilder<TSource, TProperty>(self).OnWatch(value);
 
-    public static PropertySettersIdiomBuilder<T> OnWatch<T>(this PropertySettersContext<T> self, Func<PropertySettersContext<T>, IPropertySettersBuilder<T>> configure)
-        => new PropertySettersIdiomBuilder<T>(self).OnWatch(configure);
+    public static PropertySettersIdiomBuilder<TSource, TProperty> OnWatch<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, Func<PropertySettersContext<TSource, TProperty>, IPropertySettersBuilder<TSource, TProperty>> configure)
+        => new PropertySettersIdiomBuilder<TSource, TProperty>(self).OnWatch(configure);
 
-    public static PropertySettersPlatformBuilder<T> OnMacCatalyst<T>(this PropertySettersContext<T> self, T value)
-        => new PropertySettersPlatformBuilder<T>(self).OnMacCatalyst(value);
+    public static PropertySettersPlatformBuilder<TSource, TProperty> OnMacCatalyst<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, TProperty value)
+        => new PropertySettersPlatformBuilder<TSource, TProperty>(self).OnMacCatalyst(value);
 
-    public static PropertySettersPlatformBuilder<T> OnMacCatalyst<T>(this PropertySettersContext<T> self, Func<PropertySettersContext<T>, IPropertySettersBuilder<T>> configure)
-        => new PropertySettersPlatformBuilder<T>(self).OnMacCatalyst(configure);
+    public static PropertySettersPlatformBuilder<TSource, TProperty> OnMacCatalyst<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, Func<PropertySettersContext<TSource, TProperty>, IPropertySettersBuilder<TSource, TProperty>> configure)
+        => new PropertySettersPlatformBuilder<TSource, TProperty>(self).OnMacCatalyst(configure);
 
-    public static PropertySettersPlatformBuilder<T> OniOS<T>(this PropertySettersContext<T> self, T value)
-        => new PropertySettersPlatformBuilder<T>(self).OniOS(value);
+    public static PropertySettersPlatformBuilder<TSource, TProperty> OniOS<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, TProperty value)
+        => new PropertySettersPlatformBuilder<TSource, TProperty>(self).OniOS(value);
 
-    public static PropertySettersPlatformBuilder<T> OniOS<T>(this PropertySettersContext<T> self, Func<PropertySettersContext<T>, IPropertySettersBuilder<T>> configure)
-        => new PropertySettersPlatformBuilder<T>(self).OniOS(configure);
+    public static PropertySettersPlatformBuilder<TSource, TProperty> OniOS<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, Func<PropertySettersContext<TSource, TProperty>, IPropertySettersBuilder<TSource, TProperty>> configure)
+        => new PropertySettersPlatformBuilder<TSource, TProperty>(self).OniOS(configure);
 
-    public static PropertySettersPlatformBuilder<T> OnAndroid<T>(this PropertySettersContext<T> self, T value)
-        => new PropertySettersPlatformBuilder<T>(self).OnAndroid(value);
+    public static PropertySettersPlatformBuilder<TSource, TProperty> OnAndroid<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, TProperty value)
+        => new PropertySettersPlatformBuilder<TSource, TProperty>(self).OnAndroid(value);
 
-    public static PropertySettersPlatformBuilder<T> OnAndroid<T>(this PropertySettersContext<T> self, Func<PropertySettersContext<T>, IPropertySettersBuilder<T>> configure)
-        => new PropertySettersPlatformBuilder<T>(self).OnAndroid(configure);
+    public static PropertySettersPlatformBuilder<TSource, TProperty> OnAndroid<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, Func<PropertySettersContext<TSource, TProperty>, IPropertySettersBuilder<TSource, TProperty>> configure)
+        => new PropertySettersPlatformBuilder<TSource, TProperty>(self).OnAndroid(configure);
 
-    public static PropertySettersPlatformBuilder<T> OnWinUI<T>(this PropertySettersContext<T> self, T value)
-        => new PropertySettersPlatformBuilder<T>(self).OnWinUI(value);
+    public static PropertySettersPlatformBuilder<TSource, TProperty> OnWinUI<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, TProperty value)
+        => new PropertySettersPlatformBuilder<TSource, TProperty>(self).OnWinUI(value);
 
-    public static PropertySettersPlatformBuilder<T> OnWinUI<T>(this PropertySettersContext<T> self, Func<PropertySettersContext<T>, IPropertySettersBuilder<T>> configure)
-        => new PropertySettersPlatformBuilder<T>(self).OnWinUI(configure);
+    public static PropertySettersPlatformBuilder<TSource, TProperty> OnWinUI<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, Func<PropertySettersContext<TSource, TProperty>, IPropertySettersBuilder<TSource, TProperty>> configure)
+        => new PropertySettersPlatformBuilder<TSource, TProperty>(self).OnWinUI(configure);
 
-    public static PropertySettersPlatformBuilder<T> OnTizen<T>(this PropertySettersContext<T> self, T value)
-        => new PropertySettersPlatformBuilder<T>(self).OnTizen(value);
+    public static PropertySettersPlatformBuilder<TSource, TProperty> OnTizen<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, TProperty value)
+        => new PropertySettersPlatformBuilder<TSource, TProperty>(self).OnTizen(value);
 
-    public static PropertySettersPlatformBuilder<T> OnTizen<T>(this PropertySettersContext<T> self, Func<PropertySettersContext<T>, IPropertySettersBuilder<T>> configure)
-        => new PropertySettersPlatformBuilder<T>(self).OnTizen(configure);
+    public static PropertySettersPlatformBuilder<TSource, TProperty> OnTizen<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, Func<PropertySettersContext<TSource, TProperty>, IPropertySettersBuilder<TSource, TProperty>> configure)
+        => new PropertySettersPlatformBuilder<TSource, TProperty>(self).OnTizen(configure);
 
-    public static PropertySettersThemeBuilder<T> OnLight<T>(this PropertySettersContext<T> self, T value)
-        => new PropertySettersThemeBuilder<T>(self).OnLight(value);
+    public static PropertySettersThemeBuilder<TSource, TProperty> OnLight<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, TProperty value)
+        => new PropertySettersThemeBuilder<TSource, TProperty>(self).OnLight(value);
 
-    public static PropertySettersThemeBuilder<T> OnLight<T>(this PropertySettersContext<T> self, Func<PropertySettersContext<T>, IPropertySettersBuilder<T>> configure)
-        => new PropertySettersThemeBuilder<T>(self).OnLight(configure);
+    public static PropertySettersThemeBuilder<TSource, TProperty> OnLight<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, Func<PropertySettersContext<TSource, TProperty>, IPropertySettersBuilder<TSource, TProperty>> configure)
+        => new PropertySettersThemeBuilder<TSource, TProperty>(self).OnLight(configure);
 
-    public static PropertySettersThemeBuilder<T> OnDark<T>(this PropertySettersContext<T> self, T value)
-        => new PropertySettersThemeBuilder<T>(self).OnDark(value);
+    public static PropertySettersThemeBuilder<TSource, TProperty> OnDark<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, TProperty value)
+        => new PropertySettersThemeBuilder<TSource, TProperty>(self).OnDark(value);
 
-    public static PropertySettersThemeBuilder<T> OnDark<T>(this PropertySettersContext<T> self, Func<PropertySettersContext<T>, IPropertySettersBuilder<T>> configure)
-        => new PropertySettersThemeBuilder<T>(self).OnDark(configure);
+    public static PropertySettersThemeBuilder<TSource, TProperty> OnDark<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, Func<PropertySettersContext<TSource, TProperty>, IPropertySettersBuilder<TSource, TProperty>> configure)
+        => new PropertySettersThemeBuilder<TSource, TProperty>(self).OnDark(configure);
 
-    public static PropertySettersDynamicResourcesBuilder<T> DynamicResource<T>(this PropertySettersContext<T> self, string key)
-        => new PropertySettersDynamicResourcesBuilder<T>(self).DynamicResource(key);
+    public static PropertySettersDynamicResourcesBuilder<TSource, TProperty> DynamicResource<TSource, TProperty>(this PropertySettersContext<TSource, TProperty> self, string key)
+        => new PropertySettersDynamicResourcesBuilder<TSource, TProperty>(self).DynamicResource(key);
 }

87d1572ee1900c9ad6f2223540e443ead1885c5b
gonultasmf <gonultasmf@hotmail.com>
2025-05-01 15:41:42 +0300

    WebView adds a ProcessTerminated event that's raised when a WebView process ends unexpectedly.

--- src/FmgLib.MauiMarkup/Extensions/WebViewExtension.cs
+++ src/FmgLib.MauiMarkup/Extensions/WebViewExtension.cs
@@ -5,234 +5,248 @@ public static partial class WebViewExtension
 {
     public static T CanGoBack<T>(this T self,
         bool canGoBack)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
-        self.SetValue(WebView.CanGoBackProperty, canGoBack);
+        self.SetValue(Microsoft.Maui.Controls.WebView.CanGoBackProperty, canGoBack);
         return self;
     }
 
     public static T CanGoBack<T>(this T self,
         Func<bool> configure)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
         var canGoBack = configure();
-        self.SetValue(WebView.CanGoBackProperty, canGoBack);
+        self.SetValue(Microsoft.Maui.Controls.WebView.CanGoBackProperty, canGoBack);
         return self;
     }
     
     public static T CanGoBack<T>(this T self, Func<PropertyContext<bool>, IPropertyBuilder<bool>> configure)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
-        var context = new PropertyContext<bool>(self, WebView.CanGoBackProperty);
+        var context = new PropertyContext<bool>(self, Microsoft.Maui.Controls.WebView.CanGoBackProperty);
         configure(context).Build();
         return self;
     }
     
     public static SettersContext<T> CanGoBack<T>(this SettersContext<T> self,
         bool canGoBack)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
-        self.XamlSetters.Add(new Setter { Property = WebView.CanGoBackProperty, Value = canGoBack });
+        self.XamlSetters.Add(new Setter { Property = Microsoft.Maui.Controls.WebView.CanGoBackProperty, Value = canGoBack });
         return self;
     }
     
     public static SettersContext<T> CanGoBack<T>(this SettersContext<T> self, Func<PropertySettersContext<bool>, IPropertySettersBuilder<bool>> configure)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
-        var context = new PropertySettersContext<bool>(self.XamlSetters, WebView.CanGoBackProperty);
+        var context = new PropertySettersContext<bool>(self.XamlSetters, Microsoft.Maui.Controls.WebView.CanGoBackProperty);
         configure(context).Build();
         return self;
     }
     
     public static T CanGoForward<T>(this T self,
         bool canGoForward)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
-        self.SetValue(WebView.CanGoForwardProperty, canGoForward);
+        self.SetValue(Microsoft.Maui.Controls.WebView.CanGoForwardProperty, canGoForward);
         return self;
     }
 
     public static T CanGoForward<T>(this T self,
         Func<bool> configure)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
         var canGoForward = configure();
-        self.SetValue(WebView.CanGoForwardProperty, canGoForward);
+        self.SetValue(Microsoft.Maui.Controls.WebView.CanGoForwardProperty, canGoForward);
         return self;
     }
     
     public static T CanGoForward<T>(this T self, Func<PropertyContext<bool>, IPropertyBuilder<bool>> configure)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
-        var context = new PropertyContext<bool>(self, WebView.CanGoForwardProperty);
+        var context = new PropertyContext<bool>(self, Microsoft.Maui.Controls.WebView.CanGoForwardProperty);
         configure(context).Build();
         return self;
     }
     
     public static SettersContext<T> CanGoForward<T>(this SettersContext<T> self,
         bool canGoForward)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
-        self.XamlSetters.Add(new Setter { Property = WebView.CanGoForwardProperty, Value = canGoForward });
+        self.XamlSetters.Add(new Setter { Property = Microsoft.Maui.Controls.WebView.CanGoForwardProperty, Value = canGoForward });
         return self;
     }
     
     public static SettersContext<T> CanGoForward<T>(this SettersContext<T> self, Func<PropertySettersContext<bool>, IPropertySettersBuilder<bool>> configure)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
-        var context = new PropertySettersContext<bool>(self.XamlSetters, WebView.CanGoForwardProperty);
+        var context = new PropertySettersContext<bool>(self.XamlSetters, Microsoft.Maui.Controls.WebView.CanGoForwardProperty);
         configure(context).Build();
         return self;
     }
     
     public static T Cookies<T>(this T self,
         System.Net.CookieContainer cookies)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
-        self.SetValue(WebView.CookiesProperty, cookies);
+        self.SetValue(Microsoft.Maui.Controls.WebView.CookiesProperty, cookies);
         return self;
     }
 
     public static T Cookies<T>(this T self,
         Func<System.Net.CookieContainer> configure)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
         var cookies = configure();
-        self.SetValue(WebView.CookiesProperty, cookies);
+        self.SetValue(Microsoft.Maui.Controls.WebView.CookiesProperty, cookies);
         return self;
     }
     
     public static T Cookies<T>(this T self, Func<PropertyContext<System.Net.CookieContainer>, IPropertyBuilder<System.Net.CookieContainer>> configure)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
-        var context = new PropertyContext<System.Net.CookieContainer>(self, WebView.CookiesProperty);
+        var context = new PropertyContext<System.Net.CookieContainer>(self, Microsoft.Maui.Controls.WebView.CookiesProperty);
         configure(context).Build();
         return self;
     }
     
     public static SettersContext<T> Cookies<T>(this SettersContext<T> self,
         System.Net.CookieContainer cookies)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
-        self.XamlSetters.Add(new Setter { Property = WebView.CookiesProperty, Value = cookies });
+        self.XamlSetters.Add(new Setter { Property = Microsoft.Maui.Controls.WebView.CookiesProperty, Value = cookies });
         return self;
     }
     
     public static SettersContext<T> Cookies<T>(this SettersContext<T> self, Func<PropertySettersContext<System.Net.CookieContainer>, IPropertySettersBuilder<System.Net.CookieContainer>> configure)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
-        var context = new PropertySettersContext<System.Net.CookieContainer>(self.XamlSetters, WebView.CookiesProperty);
+        var context = new PropertySettersContext<System.Net.CookieContainer>(self.XamlSetters, Microsoft.Maui.Controls.WebView.CookiesProperty);
         configure(context).Build();
         return self;
     }
     
     public static T Source<T>(this T self,
-        WebViewSource source)
-        where T : WebView
+        Microsoft.Maui.Controls.WebViewSource source)
+        where T : Microsoft.Maui.Controls.WebView
     {
-        self.SetValue(WebView.SourceProperty, source);
+        self.SetValue(Microsoft.Maui.Controls.WebView.SourceProperty, source);
         return self;
     }
 
     public static T Source<T>(this T self,
-        Func<WebViewSource> configure)
-        where T : WebView
+        Func<Microsoft.Maui.Controls.WebViewSource> configure)
+        where T : Microsoft.Maui.Controls.WebView
     {
         var source = configure();
-        self.SetValue(WebView.SourceProperty, source);
+        self.SetValue(Microsoft.Maui.Controls.WebView.SourceProperty, source);
         return self;
     }
     
-    public static T Source<T>(this T self, Func<PropertyContext<WebViewSource>, IPropertyBuilder<WebViewSource>> configure)
-        where T : WebView
+    public static T Source<T>(this T self, Func<PropertyContext<Microsoft.Maui.Controls.WebViewSource>, IPropertyBuilder<Microsoft.Maui.Controls.WebViewSource>> configure)
+        where T : Microsoft.Maui.Controls.WebView
     {
-        var context = new PropertyContext<WebViewSource>(self, WebView.SourceProperty);
+        var context = new PropertyContext<Microsoft.Maui.Controls.WebViewSource>(self, Microsoft.Maui.Controls.WebView.SourceProperty);
         configure(context).Build();
         return self;
     }
     
     public static SettersContext<T> Source<T>(this SettersContext<T> self,
-        WebViewSource source)
-        where T : WebView
+        Microsoft.Maui.Controls.WebViewSource source)
+        where T : Microsoft.Maui.Controls.WebView
     {
-        self.XamlSetters.Add(new Setter { Property = WebView.SourceProperty, Value = source });
+        self.XamlSetters.Add(new Setter { Property = Microsoft.Maui.Controls.WebView.SourceProperty, Value = source });
         return self;
     }
     
-    public static SettersContext<T> Source<T>(this SettersContext<T> self, Func<PropertySettersContext<WebViewSource>, IPropertySettersBuilder<WebViewSource>> configure)
-        where T : WebView
+    public static SettersContext<T> Source<T>(this SettersContext<T> self, Func<PropertySettersContext<Microsoft.Maui.Controls.WebViewSource>, IPropertySettersBuilder<Microsoft.Maui.Controls.WebViewSource>> configure)
+        where T : Microsoft.Maui.Controls.WebView
     {
-        var context = new PropertySettersContext<WebViewSource>(self.XamlSetters, WebView.SourceProperty);
+        var context = new PropertySettersContext<Microsoft.Maui.Controls.WebViewSource>(self.XamlSetters, Microsoft.Maui.Controls.WebView.SourceProperty);
         configure(context).Build();
         return self;
     }
     
     public static T UserAgent<T>(this T self,
         string userAgent)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
-        self.SetValue(WebView.UserAgentProperty, userAgent);
+        self.SetValue(Microsoft.Maui.Controls.WebView.UserAgentProperty, userAgent);
         return self;
     }
 
     public static T UserAgent<T>(this T self,
         Func<string> configure)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
         var userAgent = configure();
-        self.SetValue(WebView.UserAgentProperty, userAgent);
+        self.SetValue(Microsoft.Maui.Controls.WebView.UserAgentProperty, userAgent);
         return self;
     }
     
     public static T UserAgent<T>(this T self, Func<PropertyContext<string>, IPropertyBuilder<string>> configure)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
-        var context = new PropertyContext<string>(self, WebView.UserAgentProperty);
+        var context = new PropertyContext<string>(self, Microsoft.Maui.Controls.WebView.UserAgentProperty);
         configure(context).Build();
         return self;
     }
     
     public static SettersContext<T> UserAgent<T>(this SettersContext<T> self,
         string userAgent)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
-        self.XamlSetters.Add(new Setter { Property = WebView.UserAgentProperty, Value = userAgent });
+        self.XamlSetters.Add(new Setter { Property = Microsoft.Maui.Controls.WebView.UserAgentProperty, Value = userAgent });
         return self;
     }
     
     public static SettersContext<T> UserAgent<T>(this SettersContext<T> self, Func<PropertySettersContext<string>, IPropertySettersBuilder<string>> configure)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
-        var context = new PropertySettersContext<string>(self.XamlSetters, WebView.UserAgentProperty);
+        var context = new PropertySettersContext<string>(self.XamlSetters, Microsoft.Maui.Controls.WebView.UserAgentProperty);
         configure(context).Build();
         return self;
     }
     
-    public static T OnNavigated<T>(this T self, System.EventHandler<WebNavigatedEventArgs> handler)
-        where T : WebView
+    public static T OnNavigated<T>(this T self, System.EventHandler<Microsoft.Maui.Controls.WebNavigatedEventArgs> handler)
+        where T : Microsoft.Maui.Controls.WebView
     {
         self.Navigated += handler;
         return self;
     }
     
     public static T OnNavigated<T>(this T self, System.Action<T> action)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
         self.Navigated += (o, arg) => action(self);
         return self;
     }
         
-    public static T OnNavigating<T>(this T self, System.EventHandler<WebNavigatingEventArgs> handler)
-        where T : WebView
+    public static T OnNavigating<T>(this T self, System.EventHandler<Microsoft.Maui.Controls.WebNavigatingEventArgs> handler)
+        where T : Microsoft.Maui.Controls.WebView
     {
         self.Navigating += handler;
         return self;
     }
     
     public static T OnNavigating<T>(this T self, System.Action<T> action)
-        where T : WebView
+        where T : Microsoft.Maui.Controls.WebView
     {
         self.Navigating += (o, arg) => action(self);
+        return self;
+    }
+        
+    public static T OnProcessTerminated<T>(this T self, System.EventHandler<Microsoft.Maui.Controls.WebViewProcessTerminatedEventArgs> handler)
+        where T : Microsoft.Maui.Controls.WebView
+    {
+        self.ProcessTerminated += handler;
+        return self;
+    }
+    
+    public static T OnProcessTerminated<T>(this T self, System.Action<T> action)
+        where T : Microsoft.Maui.Controls.WebView
+    {
+        self.ProcessTerminated += (o, arg) => action(self);
         return self;
     }
         
